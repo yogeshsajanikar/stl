934957fc3f843e2e66448b1404596c41356c5945
Yogesh Sajanikar <yogesh_sajanikar@yahoo.com>
2014-11-24 20:29:09 +0530

    Adding TextParser as a default parser, however the problem still remains

--- stl/harness/Main.hs
+++ stl/harness/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.STL.Parser
+import Data.STL.TextParser
 import Data.STL.Topology
 import System.Environment
 
@@ -9,14 +9,14 @@ main = do
   let tolerance = 0.001 :: Float
   (path:_) <- getArgs
   putStrLn $ "Parsing STL file: " ++ path
-  putStrLn $ "Default tolerance " ++ (show tolerance)
-  s <- parseSTL2 tolerance path
+  putStrLn $ "Default tolerance " ++ show tolerance
+  s <- parseSolid tolerance path
   putStrLn "Parsing complete"
   case s of
    Left error -> putStrLn error
    Right s    -> do
-     putStrLn $ "Num points : " ++ (show $ numPoints s)
-     putStrLn $ "Num facets : " ++ (show $ numFacets s)
+     putStrLn $ "Num points : " ++ show (numPoints s)
+     putStrLn $ "Num facets : " ++ show (numFacets s)
   return 0
 
 
--- stl/src/Data/STL/TextParser.hs
+++ stl/src/Data/STL/TextParser.hs
@@ -4,42 +4,49 @@ module Data.STL.TextParser where
 
 import Control.Applicative
 import Data.Attoparsec.Text.Lazy as Al
-import Data.Text
+import Data.Text as T hiding(foldl)
+import Data.Text.Lazy.IO as TIO
+import qualified Data.Text.Lazy as Tl
 import Data.STL.Topology
+import Control.Monad
+import System.IO
 
 -- | If no solid name is given, we assume name of the solid as 
 -- "default". We ignore the solid name
 solidName :: Parser Text
 solidName = do
-  skipWhile isHorizontalSpace
-  option "default" $ takeWhile1 (not . isHorizontalSpace)
+  skipWhile isHorizontalSpace                     <?> "skip whitespace before solid name"
+  option "default" (fmap T.pack (many1 letter)    <?> "solid name parser")
 
 -- | Match start of the solid
 beginSolid' :: Parser Text
 beginSolid' = do
-  skipSpace
-  string "solid"
-  name <- solidName
-  takeTill isEndOfLine
-  endOfLine
+  skipSpace                           <?> "Neglect white space before 'solid'"
+  string "solid"                      <?> "Parse keyword 'solid'"
+  name <- solidName                   <?> "parse name of solid, optional"
+  takeTill isEndOfLine                <?> "Neglect till end of the line"
+  endOfLine                           <?> "match end of line"
   return name
 
+-- | Match beginning of the solid data, and create a solid with given tolerance
 beginSolid :: a -> Parser (Solid a)
 beginSolid tolerance =
   (\_ -> createSolid space) <$> beginSolid'
   where
     space = createSpace tolerance
 
+
 coordinate :: (Fractional a) => Parser a
 coordinate = skipWhile isHorizontalSpace *> rational
 
 coordinates :: Fractional a => Text -> Parser (a, a, a)
 coordinates s = do
+  skipSpace
   string s
   x <- coordinate
   y <- coordinate
   z <- coordinate
-  return $ (x, y, z)
+  return (x, y, z)
 
 triad :: Parser (a, a, a) -> (a -> a -> a -> b) -> Parser b
 triad p f =
@@ -60,10 +67,51 @@ facetContent = Al.count 3 $ coordinates "vertex"
 endFacet :: Parser ()
 endFacet = skipSpace *> string "endfacet" *> pure ()
 
+facet :: (Fractional a, Ord a) => Solid a -> Parser (Solid a)
 facet s = do
-  n  <- triad beginFacet (createVec s)
-  v1 <- vertexPoint s
-  return (n, v1)
+  n          <- triad beginFacet (createVec s)  <?> "Begin facet with normal"
+  skipSpace *> string "outer loop"              <?> "Outer loop start"
+  [v1,v2,v3] <- Al.count 3 (vertexPoint s)      <?> "facet vertices"
+  skipSpace *> string "endloop"                 <?> "Outer loop ends"
+  endFacet   <- endFacet                        <?> "End facet"
+  return $ addFace s v1 v2 v3 n 
   
-  
+parseFacet :: (Fractional a, Ord a) => Solid a -> Tl.Text -> Result (Solid a)
+parseFacet = parse . facet 
 
+parseFacets :: (Fractional a, Ord a) => Solid a -> Tl.Text -> Result (Solid a)
+parseFacets s i = case parseFacet s i of 
+                       Done i' s'    -> parseFacet s' i'
+                       Fail i' _ _   -> Done i' s
+
+endSolid :: Parser ()
+endSolid = skipSpace *> string "endsolid" *> pure ()
+
+-- parseBeginSolid :: a -> Tl.Text -> Either String (Solid a)
+-- parseBeginSolid tolerance i = case parse (beginSolid tolerance) i of
+--                                    Done i' 
+
+facets :: (Ord a, Fractional a) => Solid a -> Parser (Solid a)
+facets s = do
+       (t, s') <- option (True, s) $ do { s'' <- facet s; return (False, s'') }
+       if t then 
+          return s'
+       else 
+          facets s'
+
+solid :: (Ord a, Fractional a) => a -> Parser (Solid a)
+solid t = do
+      s  <- beginSolid t
+      s' <- option s (facets s) <?> "Parsing facets"
+      endSolid
+      return s'
+
+
+parseSolid tolerance path = withFile path ReadMode (\handle -> do 
+                                                        i <- TIO.hGetContents handle
+                                                        return $ eitherResult $ parse (solid tolerance) i )
+
+
+parseSolid2 tolerance path = withFile path ReadMode $ parseSolidStrict tolerance
+
+parseSolidStrict tolerance path 
